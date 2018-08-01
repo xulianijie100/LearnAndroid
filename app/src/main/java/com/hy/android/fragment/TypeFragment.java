e83d2f47044fdf3d306bae87eac26a64cd6e593d
xulianjie100 <1656829397@qq.com>
2018-08-01 16:04:04 +0800

    commit code 2018/8/1

--- app/src/main/java/com/hy/android/fragment/TypeFragment.java
+++ app/src/main/java/com/hy/android/fragment/TypeFragment.java
@@ -1,19 +1,11 @@
 package com.hy.android.fragment;
 
 import android.view.View;
-import android.widget.TextView;
 
 import com.hy.android.Base.BaseFragment;
 import com.hy.android.R;
 
-import butterknife.BindView;
-import butterknife.OnClick;
-
 public class TypeFragment extends BaseFragment implements View.OnClickListener {
-
-
-    @BindView(R.id.tv_01)
-    TextView tv_01;
 
     @Override
     protected int getLayoutId() {
@@ -29,11 +21,10 @@ public class TypeFragment extends BaseFragment implements View.OnClickListener {
     protected void initView() {
     }
 
-    @OnClick({R.id.tv_01})
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.tv_01:
-                break;
             default:
                 break;
         }
