82b73314838a99479fe11812b9add2990ded5a17
xulianjie100 <1656829397@qq.com>
2018-08-07 18:00:00 +0800

    commit code 2018/8/1

--- location/src/main/java/com/hy/location/activity/LocalActivity.java
+++ location/src/main/java/com/hy/location/activity/LocalActivity.java
@@ -1,85 +1,40 @@
 package com.hy.location.activity;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
-import com.amap.api.services.core.LatLonPoint;
-import com.amap.api.services.route.BusRouteResult;
-import com.amap.api.services.route.DrivePath;
-import com.amap.api.services.route.DriveRouteResult;
-import com.amap.api.services.route.RideRouteResult;
-import com.amap.api.services.route.RouteSearch;
-import com.amap.api.services.route.WalkRouteResult;
+import com.amap.api.services.help.Inputtips;
+import com.amap.api.services.help.InputtipsQuery;
+import com.amap.api.services.help.Tip;
 import com.hy.location.R;
 import com.hy.location.bean.LineBean;
-import com.hy.location.bean.LocalBean;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
 import io.realm.Realm;
 
 
-public class LocalActivity extends AppCompatActivity implements AMapLocationListener, RouteSearch.OnRouteSearchListener {
+public class LocalActivity extends AppCompatActivity implements Inputtips.InputtipsListener {
     private static final String TAG = "LocalActivity";
     private static final int MY_PERMISSION_REQUEST_CODE = 1000;
-    //声明AMapLocationClient类对象
-    public AMapLocationClient mLocationClient = null;
-    public AMapLocationClientOption mLocationOption = null;
-
     private EditText edit_local1;
     private EditText edit_local2;
     private Context mContext;
     private Button btn_add;
     private EditText edit_local;
     private String edit_str = "";
-
-    private RouteSearch mRouteSearch;
-    protected LatLonPoint mEndLatlng;
-    protected LatLonPoint mStartLatlng;
-    private DriveRouteResult mDriveRouteResult;
-
-    private double latitude;
-    private double longitude;
 
     private Realm mRealm;
 
@@ -90,33 +45,7 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
         setContentView(R.layout.activity_local);
 
         initToolbar();
-
         initView();
-
-        boolean granted = checkPermissionAllGranted(new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.READ_PHONE_STATE
-        });
-
-        if (granted) {
-            location();
-        }
-
-        requestPermission();
-    }
-
-    private void SearchLocal() {
-        mRouteSearch = new RouteSearch(this);
-        mRouteSearch.setRouteSearchListener(this);
-        //经度--纬度: 113.372864---22.978249
-        mStartLatlng = new LatLonPoint(Double.valueOf(latitude), Double.valueOf(longitude));
-        mEndLatlng = new LatLonPoint(Double.valueOf("24"), Double.valueOf("120"));
-        final RouteSearch.FromAndTo fromAndTo = new RouteSearch.FromAndTo(
-                mStartLatlng, mEndLatlng);
-        RouteSearch.DriveRouteQuery query = new RouteSearch.DriveRouteQuery(fromAndTo, RouteSearch.DrivingDefault, null,
-                null, "");// 第一个参数表示路径规划的起点和终点，第二个参数表示驾车模式，第三个参数表示途经点，第四个参数表示避让区域，第五个参数表示避让道路
-        mRouteSearch.calculateDriveRouteAsyn(query);// 异步路径规划驾车模式查询
     }
 
     private void initView() {
@@ -126,6 +55,29 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
         edit_local = (EditText) findViewById(R.id.edit_local);
         edit_local1 = (EditText) findViewById(R.id.edit_local_1);
         edit_local2 = (EditText) findViewById(R.id.edit_local_2);
+
+
+        edit_local1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text=s.toString();
+                if(!TextUtils.isEmpty(text)){
+                    tipsQuery(text);
+                }
+            }
+        });
+
+
 
         btn_add.setOnClickListener(v -> {
             edit_str = edit_local.getText().toString();
@@ -139,7 +91,7 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
 
             LineBean bean = new LineBean();
             bean.setRouteName(edit_str);
-            bean.setRouteName(edit_str1+"--->"+edit_str2);
+            bean.setRouteName(edit_str1 + "--->" + edit_str2);
             mRealm.beginTransaction();
             mRealm.copyToRealm(bean);
             mRealm.commitTransaction();
@@ -148,99 +100,24 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
         });
     }
 
-    @SuppressLint("HandlerLeak")
-    private Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case 100:
-                    Bundle bundle = msg.getData();
-                    String address = bundle.getString("address");
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    public static Workbook getWorkbok(File file) throws IOException {
-        Workbook wb = null;
-        FileInputStream in = new FileInputStream(file);
-        if (file.getName().endsWith("xls")) {  //Excel 2003
-            wb = new HSSFWorkbook(in);
-        } else if (file.getName().endsWith("xlsx")) {  // Excel 2007/2010
-            //wb = new XSSFWorkbook(in);
-        }
-        return wb;
+    private void tipsQuery(String str){
+        InputtipsQuery inputquery = new InputtipsQuery(str, "广州");
+        inputquery.setCityLimit(false);//限制在当前城市
+        Inputtips inputTips = new Inputtips(LocalActivity.this, inputquery);
+        inputTips.setInputtipsListener(this);
+        inputTips.requestInputtipsAsyn();
     }
 
-    private class writeDataThread extends Thread {
-
-        String path;
-
-        public writeDataThread(String xlsName) {
-            this.path = Environment.getExternalStorageDirectory().toString() + "/" + xlsName;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            OutputStream out = null;
-            File finalXlsxFile = new File(path);
-            Workbook wb = null;
-            try {
-                wb = getWorkbok(finalXlsxFile);
-                Sheet sheet = (Sheet) wb.createSheet();
-//                for (int i = 0; i < MyApplication.getList().size(); i++) {
-//                    Row nrow = sheet.createRow(i);
-//                    for (int j = 0; j < MyApplication.getList().get(i).localArray.size(); j++) {
-//                        Cell ncell = nrow.createCell(j);
-//                        ncell.setCellValue(MyApplication.getList().get(i).localArray.get(j));
-//                    }
-//                }
-                out = new FileOutputStream(path);
-                wb.write(out);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (out != null) {
-                        out.flush();
-                        out.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void onGetInputtips(List<Tip> list, int code) {
+        Log.e("code==",code+"");
+        if(list!=null && list.size()>0){
+            for(Tip tip:list){
+                Log.e("tip==",tip.getAddress());
+                Log.e("tip==",tip.getPoint().getLongitude()+"---"+tip.getPoint().getLatitude());
             }
-
-
         }
-    }
 
-    private void location() {
-        mLocationClient = new AMapLocationClient(getApplicationContext());
-        //设置定位回调监听，这里要实现AMapLocationListener接口，AMapLocationListener接口只有onLocationChanged方法可以实现，用于接收异步返回的定位结果，参数是AMapLocation类型。
-        mLocationClient.setLocationListener(this);
-        //初始化定位参数
-        mLocationOption = new AMapLocationClientOption();
-        //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //设置是否返回地址信息（默认返回地址信息）
-        mLocationOption.setNeedAddress(true);
-        //设置是否只定位一次,默认为false
-        mLocationOption.setOnceLocation(false);
-        //设置是否强制刷新WIFI，默认为强制刷新
-        mLocationOption.setWifiActiveScan(true);
-        //设置是否允许模拟位置,默认为false，不允许模拟位置
-        mLocationOption.setMockEnable(false);
-        //设置定位间隔,单位毫秒,默认为2000ms
-        mLocationOption.setInterval(2000);
-        //给定位客户端对象设置定位参数
-        mLocationClient.setLocationOption(mLocationOption);
-        //启动定位
-        mLocationClient.startLocation();
     }
 
     private void initToolbar() {
@@ -253,81 +130,6 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
         }
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.READ_PHONE_STATE,
-                        Manifest.permission.WRITE_SETTINGS
-                },
-                MY_PERMISSION_REQUEST_CODE
-        );
-    }
-
-    /**
-     * 检查是否拥有指定的所有权限
-     */
-    private boolean checkPermissionAllGranted(String[] permissions) {
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                // 只要有一个权限没有被授予, 则直接返回 false
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
-            boolean isAllGranted = true;
-
-            // 判断是否所有的权限都已经授予了
-            for (int grant : grantResults) {
-                if (grant != PackageManager.PERMISSION_GRANTED) {
-                    isAllGranted = false;
-                    break;
-                }
-            }
-
-            if (isAllGranted) {
-                // 如果所有的权限都授予了,
-                location();
-
-            } else {
-                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
-                openAppDetails();
-            }
-        }
-    }
-
-    /**
-     * 打开 APP 的详情设置
-     */
-    private void openAppDetails() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("获取位置信息需开启位置权限”，请到 “应用信息 -> 权限” 中授予！");
-        builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                startActivity(intent);
-            }
-        });
-        builder.setNegativeButton("取消", null);
-        builder.show();
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -338,92 +140,5 @@ public class LocalActivity extends AppCompatActivity implements AMapLocationList
                 break;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onLocationChanged(AMapLocation aMapLocation) {
-        if (aMapLocation != null) {
-            if (aMapLocation.getErrorCode() == 0) {
-                //定位成功回调信息，设置相关消息
-                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
-                latitude = aMapLocation.getLatitude();//获取纬度
-                longitude = aMapLocation.getLongitude();//获取经度
-                Log.e("经度--纬度", longitude + "---" + latitude);
-                aMapLocation.getAccuracy();//获取精度信息
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date date = new Date(aMapLocation.getTime());
-                df.format(date);//定位时间
-                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
-                aMapLocation.getCountry();//国家信息
-                aMapLocation.getProvince();//省信息
-                aMapLocation.getCity();//城市信息
-                aMapLocation.getDistrict();//城区信息
-                aMapLocation.getStreet();//街道信息
-                aMapLocation.getStreetNum();//街道门牌号信息
-                aMapLocation.getCityCode();//城市编码
-                aMapLocation.getAdCode();//地区编码
-
-                //获取定位信息
-                StringBuffer buffer = new StringBuffer();
-                buffer.append(aMapLocation.getCountry() + ""
-                        + aMapLocation.getProvince() + ""
-                        + aMapLocation.getCity() + ""
-                        + aMapLocation.getProvince() + ""
-                        + aMapLocation.getDistrict() + ""
-                        + aMapLocation.getStreet() + ""
-                        + aMapLocation.getStreetNum());
-                Message message = mHandler.obtainMessage();
-                Bundle bundle = new Bundle();
-                bundle.putString("address", buffer.toString());// 将服务器返回的订单号传到Bundle中，，再通过handler传出
-                message.setData(bundle);
-                message.what = 100;
-                mHandler.sendMessage(message);
-
-            } else {
-                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                Log.e("AmapError", "location Error, ErrCode:"
-                        + aMapLocation.getErrorCode() + ", errInfo:"
-                        + aMapLocation.getErrorInfo());
-                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //mLocationClient.stopLocation();//停止定位
-        mLocationClient.onDestroy();//销毁定位客户端。
-        mHandler.removeCallbacksAndMessages(null);
-    }
-
-    @Override
-    public void onDriveRouteSearched(DriveRouteResult driveRouteResult, int i) {
-
-        Log.e("result==", "" + i);
-
-
-        if (driveRouteResult != null && driveRouteResult.getPaths() != null) {
-            if (driveRouteResult.getPaths().size() > 0) {
-                DrivePath path = driveRouteResult.getPaths().get(0);
-            }
-        }
-
-    }
-
-
-    @Override
-    public void onBusRouteSearched(BusRouteResult busRouteResult, int i) {
-
-    }
-
-    @Override
-    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
-
-    }
-
-    @Override
-    public void onRideRouteSearched(RideRouteResult rideRouteResult, int i) {
-
     }
 }
