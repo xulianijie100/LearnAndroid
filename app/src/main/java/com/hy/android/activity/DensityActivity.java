6cbe2ecb5ab7835b2ccc5a16c17034da78c5119c
xulj100 <1656829397@qq.com>
2018-10-15 15:51:12 +0800

    android code commit

---
+++ app/src/main/java/com/hy/android/activity/DensityActivity.java
@@ -0,0 +1,26 @@
+package com.hy.android.activity;
+
+import com.hy.android.Base.BaseActivity;
+
+/**
+ * 屏幕适配
+ */
+
+public class DensityActivity extends BaseActivity{
+
+
+    @Override
+    public int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
